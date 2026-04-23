aca14a9c6f98439e5b1c5ad611692a26cf93ccfa
Chuanyuan <124045257+ChuanYuanNotBoat@users.noreply.github.com>
2026-04-24 02:27:39 +0800

    chore(release): bump to Beta v1.8.0 and update history

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
     {
         // Set application name and version.
         QCoreApplication::setApplicationName("Malody Catch Chart Editor");
-        QCoreApplication::setApplicationVersion("Beta v1.7.3");
+        QCoreApplication::setApplicationVersion("Beta v1.8.0");
 
         Application app(argc, argv);
 
