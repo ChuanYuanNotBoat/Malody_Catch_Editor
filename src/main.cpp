7470448e8ed99cadd27b6ee6a32b74c8a47d7a60
Chuanyuan <124045257+ChuanYuanNotBoat@users.noreply.github.com>
2026-04-26 01:26:13 +0800

    --更新文档和版本历史

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
     {
         // Set application name and version.
         QCoreApplication::setApplicationName("Malody Catch Chart Editor");
-        QCoreApplication::setApplicationVersion("Beta v1.9.0");
+        QCoreApplication::setApplicationVersion("Beta v1.9.1");
 
         Application app(argc, argv);
 
